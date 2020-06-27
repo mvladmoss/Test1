c63694051e13f01ee06cc459617b32f9b3b75d6f
uladzislau.maskoukin <gjXjB9dT.W>
2020-06-27 16:42:16 +0300

    second commit

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Version1");
+        System.out.println("Version2");
+
+        System.out.println("Version2");
     }
 }
